e645a5d3527ed95a2da5f907cf87c74efd326dce
Lavanya Kasturi <lavanyakasturi@gmail.com>
2022-10-06 09:12:24 -0500

    Added leftnav for all the products

--- sidebars.js
+++ sidebars.js
@@ -12,7 +12,7 @@
 module.exports = {
   docs: [
     'intro',
-    {
+    /*{
       type: 'category',
       label: 'Get Started',
       items: [
@@ -147,7 +147,171 @@ module.exports = {
           ],
         },
       ],
+    }, */
+
+    {
+      type: 'category',
+      label: 'Clarinet',
+      items: [
+        'clarinet/introduction',
+        'clarinet/getting-started',
+        {
+          type: 'category',
+          label: 'Feature guides',
+          items: [
+            'clarinet/feature-guides/analyze-with-check-checker',
+            'clarinet/feature-guides/use-extensions',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'How-to guides',
+          items: [
+            'clarinet/how-to-guides/how-to-set-up-local-development-environment',
+            'clarinet/how-to-guides/how-to-install-clarinet',
+            'clarinet/how-to-guides/how-to-create-new-project',
+            'clarinet/how-to-guides/how-to-add-contract',
+            'clarinet/how-to-guides/how-to-check-contract',
+            'clarinet/how-to-guides/how-to-test-contract',
+            'clarinet/how-to-guides/how-to-debug-contract',
+            'clarinet/how-to-guides/how-to-deploy-contracts',
+            'clarinet/how-to-guides/how-to-run-integration-environment',
+            'clarinet/how-to-guides/how-to-deploy-with-subnets',
+          ],
+        },
+        'clarinet/troubleshooting',
+        'clarinet/faq',
+        {
+          type: 'category',
+          label: 'Developer resources',
+          items: [
+            'clarinet/developer-resources/contribution-guidelines',
+            'clarinet/developer-resources/clarinet-discord',
+            'clarinet/developer-resources/blogs',
+            'clarinet/developer-resources/videos',
+          ],
+        },
+      ],
     },
+    {
+      type: 'category',
+      label: 'Stacks.js',
+      items: [
+        
+        'stacks.js/overview',
+        'stacks.js/getting-started',
+        {
+          type: 'category',
+          label: 'Feature guides',
+          items: [
+            'stacks.js/feature-guides/authenticate-users-with-connect',
+            'stacks.js/feature-guides/sign-messages',
+            'stacks.js/feature-guides/sign-transactions',
+            'stacks.js/feature-guides/store-data-securely',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'How-to guides',
+          items: [
+            'stacks.js/how-to-guides/how-to-integrate-stacking-delegation',
+            'stacks.js/how-to-guides/how-to-integrate-stacking',
+            'stacks.js/how-to-guides/how-to-migrate-from-blockstack.js',
+            'stacks.js/how-to-guides/how-to-use-stacks-connect-with-angular',
+          ],
+        },
+        'stacks.js/faq',
+        {
+          type: 'category',
+          label: 'Developer resources',
+          items: [
+            'stacks.js/developer-resources/contribution-guidelines',
+            'stacks.js/developer-resources/stacks.js-discord',
+            'stacks.js/developer-resources/blogs',
+            'stacks.js/developer-resources/videos',
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Stacks blockchain API',
+      items: [
+        'stacks-blockchain-api/overview',
+        'stacks-blockchain-api/getting-started',
+        {
+          type: 'category',
+          label: 'Feature guides',
+          items: [
+            'stacks-blockchain-api/feature-guides/microblocks',
+            'stacks-blockchain-api/feature-guides/nonce-handling',
+            'stacks-blockchain-api/feature-guides/openapi-spec',
+            'stacks-blockchain-api/feature-guides/pagination',
+            'stacks-blockchain-api/feature-guides/rate-limiting',
+            'stacks-blockchain-api/feature-guides/requesting-proofs',
+            'stacks-blockchain-api/feature-guides/rosetta-support',
+            'stacks-blockchain-api/feature-guides/search-endpoint',
+            'stacks-blockchain-api/feature-guides/use-clarity-values',
+            'stacks-blockchain-api/feature-guides/use-stacks-blockchain-api',
+          ]
+          },
+            {
+              type: 'category',
+              label: 'How-to guides',
+              items: [
+                'stacks-blockchain-api/how-to-guides/how-to-deploy-service-dependencies',
+                'stacks-blockchain-api/how-to-guides/how-to-handle-errors',
+                'stacks-blockchain-api/how-to-guides/how-to-install-stacks-blockchain-api',
+                'stacks-blockchain-api/how-to-guides/how-to-run-stacks-blockchain-api-docker',
+                'stacks-blockchain-api/how-to-guides/how-to-upgrade-stacks-blockchain-api',
+              ]
+              },
+              'stacks-blockchain-api/faqs',
+        {
+          type: 'category',
+          label: 'Developer resources',
+          items: [
+            'stacks-blockchain-api/developer-resources/contribution-guidelines',
+            'stacks-blockchain-api/developer-resources/stacks-blockchain-api-discord',
+            'stacks-blockchain-api/developer-resources/blogs',
+            'stacks-blockchain-api/developer-resources/videos',
+          ]
+          },
+          {
+            type: 'category',
+            label: 'Stacks Explorer',
+            items: [
+              'stacks-explorer/overview',
+              'stacks-explorer/getting-started',
+                {
+                  type: 'category',
+                  label: 'Feature guides',
+                  items: [
+                    'stacks-explorer/feature-guides/feature-guide',
+                  ]
+                  },
+                  {
+                    type: 'category',
+                    label: 'How-to guides',
+                    items: [
+                      'stacks-explorer/how-to-guides/how-to-run-build-explorer',
+                    ]
+                    },
+                    
+                    'stacks-explorer/faqs',
+                      {
+                        type: 'category',
+                        label: 'Developer resources',
+                        items: [
+                          'stacks-explorer/developer-resources/contributor-guidelines',
+                          'stacks-explorer/developer-resources/stacks-explorer-discord',
+                          'stacks-explorer/developer-resources/blogs',
+                          'stacks-explorer/developer-resources/videos',
+                        ]
+                        },
+            ],
+          }, 
+      
   ],
   tutorials: [
     'tutorials',
