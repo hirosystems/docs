cf5a4cab9ea9ede6dfa4383f7eeee1959c7ad41f
Thomas Richards <thomasrichards@Thomass-MBP.attlocal.net>
2023-02-02 22:40:24 -0800

    updated product changelog files

--- sidebars.js
+++ sidebars.js
@@ -251,6 +251,9 @@ module.exports = {
       label: 'Appendix',
       items: [
         'references/hiro-archive',
+        'references/changelog-intro',
+        'references/changelog-november',
+        'references/changelog-december',
       ],
     },
   ],
