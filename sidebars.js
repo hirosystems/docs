a0dca16d62787c8710c863715fbb7d3ceca20c47
Thomas Richards <thomasrichards@Thomass-MBP.attlocal.net>
2023-02-24 02:39:32 -0800

    added new january changelog file and updated sidebars.js file

--- sidebars.js
+++ sidebars.js
@@ -254,6 +254,7 @@ module.exports = {
         'references/hiro-archive',
         'references/changelog-november',
         'references/changelog-december',
+        'references/changelog-january',
       ],
     },
   ],
