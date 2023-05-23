553f3fcf9aeb301f8a90bb5d39e07c711477da4a
MicaiahReid <micaiahreid@gmail.com>
2023-05-23 17:05:25 -0400

    add stacks packages to ReactLiveScope

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import * as StacksNetwork from '@stacks/network';
+import * as StacksCommon from '@stacks/common';
+import * as StacksConnect from '@stacks/connect';
+import * as StacksTransactions from '@stacks/transactions';
+import * as StacksEncryption from '@stacks/encryption';
+import * as StacksProfile from '@stacks/profile';
+import * as StacksAuth from '@stacks/auth';
+import * as StacksStorage from '@stacks/storage';
+// import * as StacksWalletSDK from '@stacks/wallet-sdk';
+import * as StacksStacking from '@stacks/stacking';
+import * as StacksBlockchainApiClient from '@stacks/blockchain-api-client';
+
 const ButtonExample = props => (
   <button
     {...props}
@@ -21,6 +32,16 @@ const ReactLiveScope = {
   ...React,
   ButtonExample,
   ...StacksNetwork,
+  ...StacksCommon,
+  ...StacksConnect,
+  ...StacksTransactions,
+  ...StacksEncryption,
+  ...StacksProfile,
+  ...StacksAuth,
+  ...StacksStorage,
+  // ...StacksWalletSDK,
+  ...StacksStacking,
+  ...StacksBlockchainApiClient,
 };
 
 export default ReactLiveScope;
