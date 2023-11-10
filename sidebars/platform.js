481ecb4dfe84fe43d46ac38964b44ba5d8168d46
Ryan Waits <ryan.waits@gmail.com>
2023-11-09 23:08:26 -0600

    add custom sidebar for platform

---
+++ sidebars/platform.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    type: 'category',
+    label: 'Home',
+    type: 'link',
+    href: '/',
+    className: 'back-link'
+  },
+  {
+    type: 'html',
+    value: 'Hiro Platform',
+    className: 'section-title',
+    defaultStyle: true,
+  },
+  {
+    type: 'doc',
+    label: 'Overview',
+    id: 'platform/overview',
+  },
+  {
+    type: 'doc',
+    label: 'Getting Started',
+    id: 'platform/getting-started',
+  },
+  {
+    type: 'html',
+    value: 'Guides',
+    className: 'section-title',
+    defaultStyle: true,
+  },
+  {
+    type: 'doc',
+    label: 'Create a project',
+    id: 'platform/guides/create-project',
+  },
+  {
+    type: 'doc',
+    label: 'Build a contract',
+    id: 'platform/guides/build-contract',
+  },
+  {
+    type: 'doc',
+    label: 'Deploy a project',
+    id: 'platform/guides/deploy-project',
+  },
+  {
+    type: 'doc',
+    label: 'Archive a project',
+    id: 'platform/guides/archive-project',
+  },
+  {
+    type: 'doc',
+    label: 'Generate deployment plans',
+    id: 'platform/guides/deployment-plans',
+  },
+  {
+    type: 'doc',
+    label: 'Connect desktop to the cloud',
+    id: 'platform/guides/connect-desktop-to-cloud',
+  },
+  {
+    type: 'doc',
+    label: 'Create Chainhooks',
+    id: 'platform/guides/create-chainhooks',
+  },
+  {
+    type: 'doc',
+    label: 'Request testnet STX',
+    id: 'platform/guides/faucet',
+  },
+  {
+    type: 'html',
+    value: 'Other',
+    className: 'section-title',
+    defaultStyle: true,
+  },
+  {
+    type: 'doc',
+    label: 'FAQ',
+    id: 'platform/faq',
+  },
+]
