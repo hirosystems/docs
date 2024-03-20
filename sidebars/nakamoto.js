a7038bca9e2924e3d74546d2ea7e014a06913249
Ryan Waits <ryan.waits@gmail.com>
2024-03-20 13:49:12 -0500

    add nakamoto docs and sidebar link

---
+++ sidebars/nakamoto.js
@@ -0,0 +1,51 @@
+module.exports = [
+  {
+    type: 'category',
+    label: 'Back',
+    type: 'link',
+    href: '/',
+    className: 'back-link',
+  },
+  {
+    type: 'html',
+    value: 'Nakamoto',
+    className: 'section-title',
+    defaultStyle: true,
+  },
+  {
+    type: 'doc',
+    label: 'Overview',
+    id: 'nakamoto/index',
+  },
+  {
+    type: 'html',
+    value: 'By Product',
+    className: 'section-title',
+    defaultStyle: true,
+  },
+  {
+    type: 'doc',
+    label: 'Chainhook',
+    id: 'nakamoto/build-a-friend-tech-clone',
+  },
+  {
+    type: 'doc',
+    label: 'Clarinet',
+    id: 'nakamoto/build-a-decentralized-grants-program',
+  },
+  {
+    type: 'doc',
+    label: 'Explorer',
+    id: 'nakamoto/build-a-custom-api-using-chainhook',
+  },
+  {
+    type: 'doc',
+    label: 'Stacks.js',
+    id: 'nakamoto/build-a-custom-api-using-chainhook',
+  },
+  {
+    type: 'doc',
+    label: 'Stacks API',
+    id: 'nakamoto/build-a-custom-api-using-chainhook',
+  },
+];
