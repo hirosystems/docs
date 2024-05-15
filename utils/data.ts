631b1507f1dddaa01df44632531bb6cea44d51ca
Ryan Waits <ryan.waits@gmail.com>
2024-05-15 17:07:46 -0500

    add guides data: wip

---
+++ utils/data.ts
@@ -0,0 +1,47 @@
+export const guides = [
+  {
+    title: "How to use your API Key with Stacks.js",
+    description:
+      "Learn the steps to integrate your API key with Stacks.js for seamless application development.",
+    tags: ["stacks.js", "api keys"],
+    image:
+      "https://assets-global.website-files.com/5ff21113877dd79ed7913b57/65cd00ef6d7837b6f2cc80bd_Hiro-blog-6.jpg",
+    createdAt: new Date().toLocaleDateString(),
+  },
+  {
+    title: "Common Clarity Errors",
+    description:
+      "Identify and resolve the most frequent errors encountered while coding with Clarity.",
+    tags: ["smart contracts"],
+    image:
+      "https://assets-global.website-files.com/5ff21113877dd79ed7913b57/65e21b7984d07c8e4fb7cc7c_Hiro-blog-clarity1.jpg",
+    createdAt: new Date().toLocaleDateString(),
+  },
+  {
+    title: "How to get sBTC testnet tokens",
+    description:
+      "A guide on acquiring sBTC tokens on the testnet to help you test your applications on Stacks.",
+    tags: ["sbtc"],
+    image:
+      "https://assets-global.website-files.com/5ff21113877dd79ed7913b57/65b403b0747e9406669e4bd3_Hiro-blog-3stack.png",
+    createdAt: new Date().toLocaleDateString(),
+  },
+  {
+    title: "Host your own Ordhook Service",
+    description:
+      "Set up and run your own Ordhook service to handle blockchain webhooks with ease.",
+    tags: ["chainhook"],
+    image:
+      "https://assets-global.website-files.com/5ff21113877dd79ed7913b57/64f75dba8529c17a4d68afe0_Hiro-blog-Ordhook.jpg",
+    createdAt: new Date().toLocaleDateString(),
+  },
+  {
+    title: "Validate off-chain price feeds with Pyth",
+    description:
+      "Ensure the accuracy of your off-chain price feeds by integrating with the Pyth network.",
+    tags: ["oracles"],
+    image:
+      "https://assets-global.website-files.com/5ff21113877dd79ed7913b57/64e5fbe159b16400aed40a69_Hiro-blog-Pyth.jpg",
+    createdAt: new Date().toLocaleDateString(),
+  },
+];
