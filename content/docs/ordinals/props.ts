4c3eaab94049b9b1de7520ec730b2fac2a7bbda1
Ryan Waits <ryan.waits@gmail.com>
2024-05-20 11:35:56 -0500

    add more api references

--- content/docs/ordinals/props.ts
+++ content/docs/ordinals/props.ts
@@ -1,49 +1,48 @@
-import type { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
-import type { Callout } from 'fumadocs-ui/components/callout';
-import type { File, Folder } from 'fumadocs-ui/components/files';
-import type { InlineTOC } from 'fumadocs-ui/components/inline-toc';
-import type { RollButton } from 'fumadocs-ui/components/roll-button';
-import type { TypeTable } from 'fumadocs-ui/components/type-table';
-import type { Card } from 'fumadocs-ui/components/card';
-import type { DocsLayoutProps } from 'fumadocs-ui/layout';
-import type { AnchorHTMLAttributes, ComponentPropsWithoutRef } from 'react';
-import type { DocsPageProps } from 'fumadocs-ui/page';
-import type { RootProviderProps } from 'fumadocs-ui/provider';
-import type { AutoTypeTable } from 'fumadocs-ui/components/auto-type-table';
+import type { Accordion, Accordions } from "fumadocs-ui/components/accordion";
+import type { Callout } from "fumadocs-ui/components/callout";
+import type { File, Folder } from "fumadocs-ui/components/files";
+import type { InlineTOC } from "fumadocs-ui/components/inline-toc";
+import type { RollButton } from "fumadocs-ui/components/roll-button";
+import type { TypeTable } from "fumadocs-ui/components/type-table";
+import type { Card } from "fumadocs-ui/components/card";
+import type { DocsLayoutProps } from "fumadocs-ui/layout";
+import type { AnchorHTMLAttributes, ComponentPropsWithoutRef } from "react";
+import type { DocsPageProps } from "fumadocs-ui/page";
+import type { RootProviderProps } from "fumadocs-ui/provider";
 
 export type AccordionsProps = Omit<
   ComponentPropsWithoutRef<typeof Accordions>,
-  keyof ComponentPropsWithoutRef<'div'> | 'value' | 'onValueChange'
+  keyof ComponentPropsWithoutRef<"div"> | "value" | "onValueChange"
 >;
 
 export type AccordionProps = Omit<
   ComponentPropsWithoutRef<typeof Accordion>,
-  keyof ComponentPropsWithoutRef<'div'>
+  keyof ComponentPropsWithoutRef<"div">
 >;
 
 export type CalloutProps = Omit<
   ComponentPropsWithoutRef<typeof Callout>,
-  keyof ComponentPropsWithoutRef<'div'>
+  keyof ComponentPropsWithoutRef<"div">
 >;
 
 export type FileProps = Omit<
   ComponentPropsWithoutRef<typeof File>,
-  keyof ComponentPropsWithoutRef<'div'>
+  keyof ComponentPropsWithoutRef<"div">
 >;
 
 export type FolderProps = Omit<
   ComponentPropsWithoutRef<typeof Folder>,
-  keyof ComponentPropsWithoutRef<'div'>
+  keyof ComponentPropsWithoutRef<"div">
 >;
 
 export type InlineTOCProps = Omit<
   ComponentPropsWithoutRef<typeof InlineTOC>,
-  keyof ComponentPropsWithoutRef<'div'>
+  keyof ComponentPropsWithoutRef<"div">
 >;
 
 export type CardProps = Omit<
   ComponentPropsWithoutRef<typeof Card>,
-  keyof Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>
+  keyof Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href">
 >;
 
 export type RollButtonProps = ComponentPropsWithoutRef<typeof RollButton>;
@@ -52,22 +51,20 @@ export type TypeTableProps = ComponentPropsWithoutRef<typeof TypeTable>;
 
 export type ObjectTypeProps = ComponentPropsWithoutRef<
   typeof TypeTable
->['type'][string];
+>["type"][string];
 
 export type LayoutProps = DocsLayoutProps;
 
-export type NavbarProps = NonNullable<DocsLayoutProps['nav']>;
+export type NavbarProps = NonNullable<DocsLayoutProps["nav"]>;
 
-export type SidebarProps = NonNullable<DocsLayoutProps['sidebar']>;
+export type SidebarProps = NonNullable<DocsLayoutProps["sidebar"]>;
 
 export type PageProps = DocsPageProps;
 
-export type BreadcrumbProps = NonNullable<DocsPageProps['breadcrumb']>;
+export type BreadcrumbProps = NonNullable<DocsPageProps["breadcrumb"]>;
 
-export type TOCProps = NonNullable<DocsPageProps['tableOfContent']>;
+export type TOCProps = NonNullable<DocsPageProps["tableOfContent"]>;
 
-export type FooterProps = NonNullable<DocsPageProps['footer']>;
+export type FooterProps = NonNullable<DocsPageProps["footer"]>;
 
 export type ProviderProps = RootProviderProps;
-
-export type AutoTypeTableProps = ComponentPropsWithoutRef<typeof AutoTypeTable>;
