59dd6020827a68f23dae340854480128fc6c0db1
Ryan Waits <ryan.waits@gmail.com>
2024-05-24 07:21:16 -0500

    adjust width

--- components/layout/api-example.tsx
+++ components/layout/api-example.tsx
@@ -4,7 +4,7 @@ import { APIExample as FumaAPIExample } from "fumadocs-ui/components/api";
 
 const APIExample: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
-    <FumaAPIExample className="api-example xl:w-[47.5%]">
+    <FumaAPIExample className="api-example lg:w-[45%]">
       {children}
     </FumaAPIExample>
   );
