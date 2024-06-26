ef3592ea873e0e1e08f1cffbc06328efbcbd1a7c
Ryan Waits <ryan.waits@gmail.com>
2024-06-26 18:36:27 -0500

    update metadata

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -6,14 +6,14 @@ export function createMetadata(override: Metadata): Metadata {
     openGraph: {
       title: override.title ?? undefined,
       description: override.description ?? undefined,
-      url: "https://hiro.so",
+      url: "https://docs.hiro.so",
       images: "/og.png",
-      siteName: "Hiro",
+      siteName: "Hiro Docs",
       ...override.openGraph,
     },
     twitter: {
       card: "summary_large_image",
-      creator: "@hiro",
+      creator: "@hirosystems",
       title: override.title ?? undefined,
       description: override.description ?? undefined,
       images: "/og.png",
@@ -25,4 +25,4 @@ export function createMetadata(override: Metadata): Metadata {
 export const baseUrl =
   process.env.NODE_ENV === "development"
     ? new URL("http://localhost:3000")
-    : new URL(`https://${process.env.VERCEL_URL}`);
+    : new URL(`https://docs.hiro.so`);
