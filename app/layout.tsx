a97f618ba674beb81f13760bae1bee9d9d0a3d9b
Ryan Waits <ryan.waits@gmail.com>
2024-06-28 10:57:28 -0500

    use GoogleTagManager package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import "./global.css";
 import "fumadocs-ui/twoslash.css";
-import Script from "next/script";
 import { aeonikFono, aeonikMono, inter } from "@/app/fonts/fonts";
 import type { Viewport } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import Link from "next/link";
 import { baseUrl, createMetadata } from "@/utils/metadata";
 import { Provider } from "./provider";
+import { GoogleTagManager } from "@next/third-parties/google";
 
-const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID as string;
 
 export const metadata = createMetadata({
   title: {
@@ -38,25 +35,8 @@ export default function RootLayout({
       className={`${aeonikFono.variable} ${aeonikMono.variable} ${inter.variable}`}
       suppressHydrationWarning
     >
+      <GoogleTagManager gtmId={GTM_ID} />
       <body className="flex min-h-screen flex-col">
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
-        />
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GTM_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
         <Provider>
           {children}
           <Footer />
