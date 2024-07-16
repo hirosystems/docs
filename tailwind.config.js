d9028d68fa820a1a3e08bb1dd9342809ed2437ca
Ryan Waits <ryan.waits@gmail.com>
2024-07-16 11:30:26 -0500

    upgrade fumadocs openapi package

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     './content/**/*.mdx',
     './mdx-components.tsx',
     './node_modules/fumadocs-ui/dist/**/*.js',
+    './node_modules/fumadocs-openapi/dist/**/*.js',
   ],
   presets: [createPreset({
     preset: 'default'
