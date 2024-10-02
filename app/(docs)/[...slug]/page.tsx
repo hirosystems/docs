b3aa939f4ff1927e7de2538654315fd5c975519b
Ryan Waits <ryan.waits@gmail.com>
2024-10-02 10:36:50 -0500

    add rpc endpoints

--- app/(docs)/[...slug]/page.tsx
+++ app/(docs)/[...slug]/page.tsx
@@ -29,6 +29,7 @@ export default function Page({ params }: { params: Param }): JSX.Element {
       hacks: "Hiro Hacks",
       "clarinet-js-sdk": "Clarinet JS SDK",
       "platform-api": "Platform API",
+      "rpc-api": "Stacks Core RPC",
     };
 
     if (page.file?.name === "index" && page.slugs[1]) {
