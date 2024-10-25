8a266a04d401eb71d1f2dab967ba237b1ad79655
Ryan Waits <ryan.waits@gmail.com>
2024-10-25 14:31:54 -0500

    adjust some copy and styling

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,11 +6,11 @@ import Link from "fumadocs-core/link";
 export default function HomePage(): JSX.Element {
   return (
     <main className="container max-w-7xl mx-auto my-12 space-y-10">
-      <div className="space-y-2">
+      <div className="space-y-1">
         <h1 className="text-4xl font-bold text-gray-900">
           Welcome to Hiro Docs.
         </h1>
-        <h2 className="text-4xl font-bold text-gray-900">
+        <h2 className="text-2xl font-bold text-muted-foreground">
           Explore our tutorials, guides, API references, and more.
         </h2>
       </div>
@@ -26,9 +26,7 @@ export default function HomePage(): JSX.Element {
               <StacksCardIcon />
             </div>
             <h3 className="text-xl font-semibold mb-2">Stacks Docs</h3>
-            <p className="text-muted-foreground">
-              Start building on Stacks.
-            </p>
+            <p className="text-muted-foreground">Start building on Stacks.</p>
           </div>
 
           <div className="flex flex-wrap gap-2">
@@ -86,8 +84,7 @@ export default function HomePage(): JSX.Element {
             </div>
             <h3 className="text-xl font-semibold mb-2">Bitcoin Docs</h3>
             <p className="text-muted-foreground">
-              Start building on Ordinals
-              and Runes.
+              Start building on Ordinals and Runes.
             </p>
           </div>
           <div className="flex flex-wrap gap-2">
@@ -114,7 +111,7 @@ export default function HomePage(): JSX.Element {
       </div>
 
       <main className="space-y-6">
-        <div className="space-y-2">
+        <div className="space-y-1">
           <h2 className="text-2xl font-bold">Need help getting started?</h2>
           <p className="text-xl text-muted-foreground">
             Check out these resources.
@@ -122,42 +119,44 @@ export default function HomePage(): JSX.Element {
         </div>
 
         <div className="grid md:grid-cols-4 gap-6">
-          <Link href="/stacks/get-started" className="block">
-            <Card className="p-6 border bg-card">
+          <Link href="/stacks/get-started" className="block h-full">
+            <Card className="p-6 border bg-card h-full flex flex-col">
               <h3 className="text-lg font-semibold mb-2">
                 Get started with Stacks
               </h3>
-              <p className="text-muted-foreground text-sm">
-                Learn how to build on Stacks with some of our most popular guides and tutorials.
+              <p className="text-muted-foreground text-sm flex-grow">
+                Build on Stacks with some of our most popular guides and
+                tutorials.
               </p>
             </Card>
           </Link>
-          <Link href="/stacks/api" className="block">
-            <Card className="p-6 border bg-card">
+          <Link href="/stacks/api" className="block h-full">
+            <Card className="p-6 border bg-card h-full flex flex-col">
               <h3 className="text-lg font-semibold mb-2">
                 Stacks API Overview
               </h3>
-              <p className="text-muted-foreground text-sm">
+              <p className="text-muted-foreground text-sm flex-grow">
                 View the API reference for the Stacks API.
               </p>
             </Card>
           </Link>
-          <Link href="/bitcoin/get-started" className="block">
-            <Card className="p-6 border bg-card">
+          <Link href="/bitcoin/get-started" className="block h-full">
+            <Card className="p-6 border bg-card h-full flex flex-col">
               <h3 className="text-lg font-semibold mb-2">
                 Get started with Bitcoin
               </h3>
-              <p className="text-muted-foreground text-sm">
-                Learn how to build on Bitcoin with some of our most popular guides and tutorials.
+              <p className="text-muted-foreground text-sm flex-grow">
+                Build on Bitcoin with some of our most popular guides and
+                tutorials.
               </p>
             </Card>
           </Link>
-          <Link href="/bitcoin/ordinals/api" className="block">
-            <Card className="p-6 border bg-card">
+          <Link href="/bitcoin/ordinals/api" className="block h-full">
+            <Card className="p-6 border bg-card h-full flex flex-col">
               <h3 className="text-lg font-semibold mb-2">
                 Ordinals API Overview
               </h3>
-              <p className="text-muted-foreground text-sm">
+              <p className="text-muted-foreground text-sm flex-grow">
                 View the API reference for our Ordinals API.
               </p>
             </Card>
