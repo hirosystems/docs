3ee6634cf503027adcda41cb0c2f173ab997f792
Ryan Waits <ryan.waits@gmail.com>
2025-07-01 17:24:23 -0500

    hide project templates

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -24,12 +24,12 @@ export default function ResourcesPage() {
               title="Guides"
               description="Guides for building on Stacks and Bitcoin."
             />
-            <IndexCard
+            {/* <IndexCard
               icon={<Braces />}
               href="/resources/templates"
               title="Project templates"
               description="Project templates for building on Stacks and Bitcoin."
-            />
+            /> */}
             <IndexCard
               icon={<Code />}
               href="/resources/snippets"
